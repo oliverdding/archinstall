d72561eb0bdca967397234cf39bf873030ad107e
oliver ding <oliverdding@outlook.com>
2023-09-27 23:13:51 +0800

    add yazi

--- install-extra.sh
+++ install-extra.sh
@@ -20,7 +20,7 @@ install_package helix just taplo yaml-language-server bash-language-server marks
 install_package nodejs npm prettier
 install_package python python-setuptools python-pip python-lsp-server
 install_package rustup cargo-update
-install_package gopass bottom miniserve xplr neomutt ncmpcpp proxychains-ng jq tidy
+install_package gopass bottom miniserve yazi unarchiver ffmpegthumbnailer fd neomutt ncmpcpp proxychains-ng jq tidy
 install_package wqy-microhei wqy-bitmapfont wqy-zenhei adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts adobe-source-code-pro-fonts adobe-source-sans-pro-fonts adobe-source-serif-pro-fonts noto-fonts noto-fonts-cjk ttf-jetbrains-mono-nerd
 
 echo -e "\n### adding archlinuxcn"
